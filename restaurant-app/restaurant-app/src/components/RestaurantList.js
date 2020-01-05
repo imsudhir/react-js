19bffc9fd445f1728016a36199f009dedad32a2d
imsudhir <isudhir.developer@gmail.com>
2020-01-05 12:41:05 +0530

    Create Fake Api and fetch Data
    Fetch restaturant list from Api

--- restaurant-app/restaurant-app/src/components/RestaurantList.js
+++ restaurant-app/restaurant-app/src/components/RestaurantList.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 
 class RestaurantList extends Component {
-    
+    constructor(){
+        super();
+        this.state = {
+            list:null,
+        }
+    }
+    componentDidMount(){
+        fetch("http://localhost:3000/restaurant")
+        .then((res) => res.json()
+        .then((result) => {
+            console.log(result)  
+            this.setState(
+                {
+                list:result
+                }
+            )
+        })
+        )
+    }
     render() {
         return (
             <div>
-                <h1> Restaurent list</h1>
+                <h1> Restaurant list</h1>
+                {
+                    this.state.list ? (
+                      this.state.list.map((lists) => 
+                      {
+                    return  (<div>
+                        <h3>Name:  {lists.name} </h3><h5>email: {lists.email}</h5>
+                         </div>)
+                      })  
+                    ) :'Please wait...'
+                }
                 
             </div>
-        );
+        ); 
     }
 }
 
